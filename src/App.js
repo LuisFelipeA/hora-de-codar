469cfb66623ff59071d40dc0bc7479be04d19da4
Luis Felipe <felipeadriani2@gmail.com>
2023-05-18 14:08:55 -0300

    Criando Componentes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
+
+function sum(a, b){
+  return a + b
+}
+
+const url = 'https://via.placeholder.com/150'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Soma{sum(3, 2)}</h1>
+      <img src={url} alt="Minha Imagem"></img>
+      <Frase />
+      <HelloWorld />
+      <Frase />
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>Frase qualquer</p>
+        </div>
+    )
+}
+
+export default Frase
