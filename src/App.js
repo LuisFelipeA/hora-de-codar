66c519e2e77752165b1dea697be5a4775f0a77c4
Luis Felipe <felipeadriani2@gmail.com>
2023-07-18 15:08:19 -0300

    Aula 05

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './components/HelloWorld';
 import Frase from './components/Frase';
+import SayMayName from './components/SayMayName';
+import Pessoa from './components/Pessoa';
 
 function sum(a, b){
   return a + b
@@ -10,12 +12,17 @@ function sum(a, b){
 const url = 'https://via.placeholder.com/150'
 
 function App() {
+  const nome = 'Luis Felipe'
   return (
     <div className="App">
       <h1>Soma{sum(3, 2)}</h1>
       <img src={url} alt="Minha Imagem"></img>
       <HelloWorld />
       <Frase />
+      <SayMayName nome="Luis"/>
+      <SayMayName nome="Felipe"/>
+      <SayMayName nome={nome}/>
+      <Pessoa nome="Maria" idade="22" profissao="Programador(a)" foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
